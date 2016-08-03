a49b898bd09b4db060fcd73e80ba4d0942ec4ccd
Luna Mendes <lkmnds@gmail.com>
2016-08-03 20:20:11 -0300

    port jmusic to 3.5 syntax

--- ext/josemusic.py
+++ ext/josemusic.py
@@ -1,7 +1,12 @@
+import sys
 import asyncio
-import discord
-from discord.ext import commands
+sys.path.append("..")
 
+import discord
+import youtube_dl
+
+import josecommon as jcommon
+import joseerror as je
 MUSIC_ROLE = 'music'
 
 # sanity test
@@ -50,42 +55,39 @@ class VoiceState:
     def player(self):
         return self.current.player
 
-    @asyncio.coroutine
-    def skip(self):
+    async def skip(self):
         self.skip_votes.clear()
         if self.is_playing():
             try:
                 self.current.player.stop()
                 self.songlist.pop()
             except Exception as e:
-                yield from self.jm.say('pyerr: %s' % e)
+                await self.say('pyerr: %s' % e)
         else:
-            yield from self.jm.say("c_skip: não estou tocando nada")
+            await self.say("c_skip: não estou tocando nada")
 
     def toggle_next(self):
         self.play_next_song.set()
 
-    @asyncio.coroutine
-    def player_task(self):
+    async def player_task(self):
         while self.taskflag:
             self.play_next_song.clear()
-            self.current = yield from self.songs.get()
+            self.current = await self.songs.get()
             try:
                 self.songlist.pop()
             except Exception as e:
-                yield from self.jm.say('pyerr: %s' % e)
+                await self.jm.say('pyerr: %s' % e)
 
-            yield from self.jm.say('ZELAO® tocando: %s' % str(self.current))
+            await self.jm.say('ZELAO® tocando: %s' % str(self.current))
             self.current.player.start()
-            yield from self.play_next_song.wait()
+            await self.play_next_song.wait()
 
-class JoseMusic:
+class JoseMusic(jcommon.Extension):
     def __init__(self, cl):
-
+        jcommon.Extension.__init__(self, cl)
         self.voice_channel = discord.Object(id='208768258818441219') # funk
 
         # stuff
-        self.client = cl
         self.c_message = ''
         self.player = None
         self.state = None
@@ -100,12 +102,7 @@ class JoseMusic:
 
         return self.state
 
-    @asyncio.coroutine
-    def say(self, msg):
-        yield from self.client.send_message(self.c_message.channel, msg)
-
-    @asyncio.coroutine
-    def c_status(self, msg):
+    async def c_mstat(self, msg, args):
         res = ''
         res += 'JMusic iniciado: %s\n' % self.init_flag
         if self.init_flag and self.state:
@@ -113,33 +110,31 @@ class JoseMusic:
             res += 'tocando música? %s\n' % ispl
             if ispl:
                 res += 'Música sendo tocada: %s\n' % self.state.current
-        yield from self.say("%s" % res)
+        await self.say("%s" % res)
 
-    @asyncio.coroutine
-    def c_init(self, msg):
-        auth = yield from check_roles(MUSIC_ROLE, msg.author.roles)
+    async def c_minit(self, msg, args):
+        auth = await check_roles(MUSIC_ROLE, msg.author.roles)
         if not auth:
-            yield from self.say("jm_PermissionError: usuário não autorizado")
+            await self.say("jm_PermissionError: usuário não autorizado")
             return
 
         if self.init_flag:
-            yield from self.say("JMusic já conectado")
+            await self.say("JMusic já conectado")
             return
 
-        voice = yield from self.client.join_voice_channel(self.voice_channel)
+        voice = await self.client.join_voice_channel(self.voice_channel)
         state = self.get_voice_state()
         state.voice = voice
         self.init_flag = True
 
-    @asyncio.coroutine
-    def c_play(self, message):
-        auth = yield from check_roles(MUSIC_ROLE, message.author.roles)
+    async def c_play(self, message, args):
+        auth = await check_roles(MUSIC_ROLE, message.author.roles)
         if not auth:
-            yield from self.say("jm_PermissionError: usuário não autorizado")
+            await self.say("jm_PermissionError: usuário não autorizado")
             return
 
         if not self.init_flag:
-            yield from self.say("JMusic não foi iniciado")
+            await self.say("JMusic não foi iniciado")
             return
 
         state = self.state
@@ -151,64 +146,59 @@ class JoseMusic:
         try:
             args = message.content.split(' ')
             song = ' '.join(args[1:])
-            player = yield from state.voice.create_ytdl_player(song, ytdl_options=opts, after=state.toggle_next)
+            player = await state.voice.create_ytdl_player(song, ytdl_options=opts, after=state.toggle_next)
         except Exception as e:
-            yield from self.say("jm_err: pyerr: `%s`" % e)
+            await self.say("jm_err: pyerr: `%s`" % e)
         else:
             player.volume = 0.6
             entry = Entry(message, player)
-            yield from self.say('Colocado na fila: %s' % entry)
+            await self.say('Colocado na fila: %s' % entry)
             state.songlist.append(entry)
-            yield from state.songs.put(entry)
+            await state.songs.put(entry)
 
-    @asyncio.coroutine
-    def c_pause(self, message):
-        auth = yield from check_roles(MUSIC_ROLE, message.author.roles)
+    async def c_pause(self, message, args):
+        auth = await check_roles(MUSIC_ROLE, message.author.roles)
         if not auth:
-            yield from self.say("jm_PermissionError: usuário não autorizado")
+            await self.say("jm_PermissionError: usuário não autorizado")
             return
 
         if self.state.is_playing():
             player = self.state.current.player
             player.pause()
 
-    @asyncio.coroutine
-    def c_resume(self, message):
-        auth = yield from check_roles(MUSIC_ROLE, message.author.roles)
+    async def c_resume(self, message, args):
+        auth = await check_roles(MUSIC_ROLE, message.author.roles)
         if not auth:
-            yield from self.say("jm_PermissionError: usuário não autorizado")
+            await self.say("jm_PermissionError: usuário não autorizado")
             return
 
         if self.state.is_playing():
             player = self.state.current.player
             player.resume()
 
-    @asyncio.coroutine
-    def c_queue(self, message):
-        auth = yield from check_roles(MUSIC_ROLE, message.author.roles)
+    async def c_queue(self, message, args):
+        auth = await check_roles(MUSIC_ROLE, message.author.roles)
         if not auth:
-            yield from self.say("jm_PermissionError: usuário não autorizado")
+            await self.say("jm_PermissionError: usuário não autorizado")
             return
         res = 'Lista de sons: \n'
         for song in self.state.songlist:
             res += ' * %s\n' % song
-        yield from self.say(res)
+        await self.say(res)
 
-    @asyncio.coroutine
-    def c_playing(self, message):
+    async def c_playing(self, message, args):
         if self.loop_started:
             if self.state.is_playing():
-                yield from self.say('Música sendo tocada: %s\n' % self.state.current)
+                await self.say('Música sendo tocada: %s\n' % self.state.current)
             else:
-                yield from self.say("Nenhuma música sendo tocada")
+                await self.say("Nenhuma música sendo tocada")
         else:
-            yield from self.say("Loop não iniciado")
+            await self.say("Loop não iniciado")
 
-    @asyncio.coroutine
-    def c_stop(self, message):
-        auth = yield from check_roles(MUSIC_ROLE, message.author.roles)
+    async def c_stop(self, message, args):
+        auth = await check_roles(MUSIC_ROLE, message.author.roles)
         if not auth:
-            yield from self.say("jm_PermissionError: usuário não autorizado")
+            await self.say("jm_PermissionError: usuário não autorizado")
             return
         state = self.state
 
@@ -218,56 +208,48 @@ class JoseMusic:
 
         try:
             state.taskflag = False
-            yield from self.state.voice.disconnect()
+            await self.state.voice.disconnect()
             del self.state
         except Exception as e:
-            yield from self.say("c_stop: pyerr: %s" % e)
+            await self.say("c_stop: pyerr: %s" % e)
 
-    @asyncio.coroutine
-    def c_zelao(self, message):
-        auth = yield from check_roles(MUSIC_ROLE, message.author.roles)
+    async def c_zelao(self, message, args):
+        auth = await check_roles(MUSIC_ROLE, message.author.roles)
         if not auth:
-            yield from self.say("jm_PermissionError: usuário não autorizado")
+            await self.say("jm_PermissionError: usuário não autorizado")
             return
         if not self.loop_started:
             self.loop_started = True
-            yield from self.state.player_task()
-            yield from self.say("o player terminou... isso não deveria acontecer... né?")
+            await self.state.player_task()
+            await self.say("o player terminou... isso não deveria acontecer... né?")
         else:
-            yield from self.say("Player já iniciado")
+            await self.say("Player já iniciado")
 
-    @asyncio.coroutine
-    def c_skip(self, message):
+    async def c_skip(self, message, args):
         if not self.state.is_playing():
-            yield from self.say('Nenhuma música sendo tocada para pular')
+            await self.say('Nenhuma música sendo tocada para pular')
             return
 
         voter = message.author
         state = self.state
         if voter == state.current.requester:
-            yield from self.say('Solicitante pediu para pular a música...')
-            yield from state.skip()
+            await self.say('Solicitante pediu para pular a música...')
+            await state.skip()
             return
 
         elif voter.id not in state.skip_votes:
             state.skip_votes.add(voter.id)
             total_votes = len(state.skip_votes)
             if total_votes >= 3:
-                yield from self.say('3 votos, pulando...')
-                yield from state.skip()
+                await self.say('3 votos, pulando...')
+                await state.skip()
                 return
             else:
-                yield from self.say('Voto adicionado, já existem [{}/3]'.format(total_votes))
+                await self.say('Voto adicionado, já existem [{}/3]'.format(total_votes))
         else:
-            yield from self.say('Você já votou.')
+            await self.say('Você já votou.')
 
-    @asyncio.coroutine
-    def recv(self, msg):
-        self.c_message = msg
-
-jm = JoseMusic(None)
-
-cmds_start = {
+'''cmds_start = {
     '!mstat': jm.c_status,
     '!minit': jm.c_init,
 
@@ -282,3 +264,4 @@ cmds_start = {
     '!skip': jm.c_skip,
     '!playing': jm.c_playing,
 }
+'''
