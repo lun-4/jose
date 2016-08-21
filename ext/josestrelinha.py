7e9e7b7bad6d29e8e725e89fe93bfdd829b2f272
Luna Mendes <lkmnds@gmail.com>
2016-08-21 15:42:41 -0300

    working on like

--- ext/josestrelinha.py
+++ ext/josestrelinha.py
@@ -9,11 +9,12 @@ import josecommon as jcommon
 import joseerror as je
 
 import time
+import datetime
 import traceback
 
 STARBOARD_HELPTEXT = '''
 O Starboard do josé permite aos usuários darem like nas mensagens, sem precisar de Pin ou algo do tipo.
-`!like <message ID>` - laik :top:
+`!like <message ID>` - manda aquele laik :top:
 '''
 
 class JoseStrelinha(jcommon.Extension):
@@ -37,7 +38,7 @@ class JoseStrelinha(jcommon.Extension):
             return True
         except Exception as e:
             if self.current is not None:
-                await self.debug("load_stars: erro carregando josememes.db(%s)" % e)
+                await self.debug("load_stars: erro carregando estrelinha.db(%s)" % e)
                 return False
             else:
                 print('load_stars: erro: %s' % e)
@@ -67,6 +68,34 @@ class JoseStrelinha(jcommon.Extension):
     async def c_htstb(self, message, args):
         await self.say(STARBOARD_HELPTEXT)
 
+    def make_emoji(self, likes):
+        if likes > 0:
+            return ':ok_hand:'
+        elif likes > 2:
+            return ':star:'
+        elif likes > 4:
+            return ':star2:'
+
+    async def make_message(self, msg, likes):
+        emoji = self.make_emoji(likes)
+        content = msg.clean_content
+
+        if msg.attachments:
+            attachments = '(attachment: {[url]})'.format(msg.attachments[0])
+            if content:
+                content = content + ' ' + attachments
+            else:
+                content = attachments
+
+        # <emoji> <star> <content> - <time> by <user> in <channel>
+        if likes > 1:
+            base = '{0} **{1}**'
+        else:
+            base = '{0}'
+
+        fmt = base + ' {2} - {3.timestamp:%Y-%m-%d %H:%M UTC} by {3.author} in {3.channel.mention} (ID: {3.id})'
+        return fmt.format(emoji, likes, content, msg)
+
     async def c_like(self, message, args):
         '''`!like` - O Starboard do josé
 
@@ -95,12 +124,13 @@ class JoseStrelinha(jcommon.Extension):
         db = self.stars.get(message.server.id, {})
         starrer = message.author
 
-        msg = await self.get_message(ctx.message.channel, msg_id)
+        msg = await self.client.get_message(message.channel, msg_id)
         if msg is None:
             await self.bot.say(':question: Mensagem não encontrada')
             return
 
-        starrers = db.get(message, [])
+        stars = db.get(message, [None, []])
+        starrers = stars[1]
         if starrer.id in starrers:
             await self.say("Você já deu like nesta mensagem")
             return
@@ -113,6 +143,39 @@ class JoseStrelinha(jcommon.Extension):
             await self.say(':busstop: Você não pode dar like nas mensagens da #estrelinha')
             return
 
-        # self.stars[message.server.id] = db
+        seven_days_ago = datetime.datetime.utcnow() - datetime.timedelta(days=7)
+        if msg.timestamp < seven_days_ago:
+            await self.say(':busstop: Esta mensagem tem mais de 7 dias.')
+            return
+
+        to_send = await self.make_message(msg, len(starrers) + 1)
+        if len(to_send) > 2000:
+            await self.say(':busstop: Mensagem muito grande.')
+            return
+
+        try:
+            await self.client.delete_message(message)
+        except:
+            pass
+
+        starrers.append(starrer.id)
+        db[message] = stars
+
+        # freshly starred
+        if stars[0] is None:
+            sent = await self.client.send_message(star_channel, to_send)
+            stars[0] = sent.id
+
+            db[message] = stars
+            self.stars[message.server.id] = db
+            await self.save_stars()
+            return
+
+        bot_msg = await self.client.get_message(starboard, stars[0])
+
+        await self.bot.edit_message(bot_msg, to_send)
+        self.stars[message.server.id] = db
+
         await self.save_stars()
+
         return
--- josecommon.py
+++ josecommon.py
@@ -11,8 +11,8 @@ random = SystemRandom()
 
 import joseerror as je
 
-JOSE_VERSION = '0.8.8'
-JOSE_BUILD = 375
+JOSE_VERSION = '0.8.9'
+JOSE_BUILD = 380
 
 APP_CLIENT_ID = 'ID DO JOSE AQUI'
 OAUTH_URL = 'https://discordapp.com/oauth2/authorize?client_id=%s&scope=bot&permissions=103988231' % APP_CLIENT_ID
