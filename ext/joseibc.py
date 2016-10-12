7b91f14d66068e693841160b970418aa829fa389
Luna Mendes <lkmnds@gmail.com>
2016-10-12 13:19:02 -0300

    add documentation for ibc and add syscall 0

--- ext/joseibc.py
+++ ext/joseibc.py
@@ -19,11 +19,26 @@ class JoseIBC(jaux.Auxiliar):
     async def ext_unload(self):
         pass
 
+    async def c_callpage(self, message, args):
+        '''!callpage <num> - mostra a página da lista de syscalls'''
+        return
+
     async def c_syscall(self, message, args):
         '''`!syscall <JSON>` - https://github.com/lkmnds/jose/blob/master/doc/ibc.md'''
+        json_to_parse = ' '.join(args[1:])
         try:
-            json_data = json.loads(args[1])
+            json_data = json.loads(json_to_parse)
         except Exception as e:
             await self.say("syscall->pyerr: `%r`" % e)
             return
+
+        try:
+            syscall_number = json_data['callnumber']
+        except Exception as e:
+            await self.say("syscall->pyerr: `%r`" % e)
+            return
+
+        if syscall_number == 0:
+            await self.say("pong")
+
         return
