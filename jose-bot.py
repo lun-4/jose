a53315f831ffc6e97d96f19e426daafab335a30c
Luna Mendes <lkmnds@gmail.com>
2017-02-04 22:24:52 -0300

    fix typo

--- jose-bot.py
+++ jose-bot.py
@@ -261,7 +261,7 @@ for modname in jose.modules:
                 event_table[evname].append(handler)
 
 async def do_event(event_name, message):
-    for handler in event_table[event_table]:
+    for handler in event_table[event_name]:
         await handler(message, jcommon.Context(client, message))
 
 async def check_message(message):
