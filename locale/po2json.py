7e6776dc85829fd6d09629bfd58f9f81b5fe44be
Luna Mendes <lkmnds@gmail.com>
2017-02-11 20:16:44 -0300

    locale: add jose.json

--- locale/po2json.py
+++ locale/po2json.py
@@ -1,5 +1,6 @@
 import sys
 import polib
+import json
 
 def main(args):
     infile = args[1]
@@ -12,7 +13,7 @@ def main(args):
     print('{')
     for entry in po:
         # best
-        print("\t%r: \"%s\"," % (entry.msgid, entry.msgstr))
+        print("\t%s: \"%s\"," % (json.dumps(entry.msgid), entry.msgstr))
     print('}')
 
 if __name__ == '__main__':
