41d53813246c248308c44d11c805a0237c3a5f21
Luna Mendes <lkmnds@gmail.com>
2017-02-15 21:01:42 -0300

    backup.bash: change current directory when backuping

--- backup.bash
+++ backup.bash
@@ -6,6 +6,7 @@ JOSE_BACKUP_DIR="0ByEq2iIt9GXoWVF2YkNuMFMyMU0"
 DATE=$(date +%d-%m-%y-%H-%M)
 BACKUPFILE="jose-backup-$DATE.tar.gz"
 
-tar cvzf $BACKUPFILE db/* jose.db db/zelao.txt ext/*.db jcoin/*.db jcoin/*.journal db/jose-data.txt
+cd ~/jose
+tar cvzf $BACKUPFILE db/* jose.db db/zelao.txt ext/*.db jcoin/*.db jcoin/*.journal
 
 gdrive upload --parent $JOSE_BACKUP_DIR $BACKUPFILE
