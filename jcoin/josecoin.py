fb4b1cdc0cb1c8203d88a2f887a8b14122b9542f
Luna Mendes <lkmnds@gmail.com>
2017-02-17 16:52:20 -0300

    jcoin: port to english

--- jcoin/josecoin.py
+++ jcoin/josecoin.py
@@ -45,14 +45,14 @@ def new_acc(id_acc, name, init_amnt=None):
         init_amnt = decimal.Decimal('3')
 
     if id_acc in data:
-        return False, 'conta já existe'
+        return False, 'account already exists'
 
     data[id_acc] = empty_acc(name, init_amnt)
-    return True, 'conta criada com sucesso'
+    return True, 'account made with success'
 
 def get(id_acc):
     if id_acc not in data:
-        return False, 'conta não existe'
+        return False, 'account doesn\'t exist'
     return True, data[id_acc]
 
 def gen():
@@ -64,16 +64,16 @@ def transfer(id_from, id_to, amnt, file_name):
     amnt = decimal.Decimal(str(amnt))
 
     if amnt < 0:
-        return False, "valores menores do que zero não são permitidos"
+        return False, "values less than zero aren't permitted"
 
     if amnt == 0:
-        return False, "não é permitido enviar zero"
+        return False, "sending zero is prohibited"
 
     if not (id_from in data):
-        return False, 'conta para extrair fundos não existe'
+        return False, "account to get funds doesn't exist"
 
     if not (id_to in data):
-        return False, 'conta de destino não existe'
+        return False, "account to send funds doesn't exist"
 
     try:
         acc_from = data[id_from]
@@ -83,14 +83,14 @@ def transfer(id_from, id_to, amnt, file_name):
 
     print('from', acc_from, 'to', acc_to, 'amount', amnt)
     if not (acc_from['amount'] >= amnt):
-        return False, "conta não possui fundos suficientes para a transação"
+        return False, "account doesn't have enough funds to make this transaction"
 
     acc_to['amount'] += amnt
     acc_from['amount'] -= amnt
 
     ledger_data(file_name, "%f;TR;%s;%s;%s\n" % (time.time(), id_from, id_to, amnt))
 
-    return True, "%s foram enviados de %s para %s" % (amnt, acc_from['name'], acc_to['name'])
+    return True, "%s was sent from %s to %s" % (amnt, acc_from['name'], acc_to['name'])
 
 def load(fname):
     global data
@@ -115,8 +115,7 @@ def save(fname):
     #ledger_data(fname.replace('db', 'journal'), '%f;SAVE;%r\n' % (time.time(), data))
     return True, "save %s" % fname
 
-@asyncio.coroutine
-def jcoin_control(userid, amount):
+async def jcoin_control(userid, amount):
     return transfer(userid, jose_id, amount, LEDGER_PATH)
 
 async def raw_save():
@@ -157,7 +156,7 @@ class JoseCoin(jcommon.Extension):
             accdata = res[1]
             await cxt.say(('%s -> %.2f' % (accdata['name'], accdata['amount'])))
         else:
-            await cxt.say('conta não encontrada(`id:%s`)' % (id_check))
+            await cxt.say('account not found(`id:%s`)' % (id_check))
 
     async def c_conta(self, message, args, cxt):
         print("new jcoin account %s" % message.author.id)
@@ -177,7 +176,7 @@ class JoseCoin(jcommon.Extension):
         new_amount = decimal.Decimal(args[2])
 
         data[id_from]['amount'] = new_amount
-        await cxt.say("<@%s> tem %.2fJC agora" % (id_from, data[id_from]['amount']))
+        await cxt.say("<@%s> has %.2fJC now" % (id_from, data[id_from]['amount']))
 
     async def c_enviar(self, message, args, cxt):
         '''`!enviar @mention quantidade` - envia JCoins para uma conta'''
@@ -190,7 +189,7 @@ class JoseCoin(jcommon.Extension):
         try:
             amount = decimal.Decimal(args[2])
         except ValueError:
-            await cxt.say("ValueError: erro parseando o valor")
+            await cxt.say("ValueError: error parsing value")
             return
         except Exception as e:
             await cxt.say("Exception: `%r`" % e)
@@ -220,7 +219,7 @@ class JoseCoin(jcommon.Extension):
         }
 
         if range_max >= 16:
-            await cxt.say("LimitError: valores maiores do que 16 não válidos")
+            await cxt.say("LimitError: values higher than 16 aren't valid")
             return
 
         order = []
