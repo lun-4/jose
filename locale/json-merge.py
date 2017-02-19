5413a31e971702941b8580b475bc98a408c72dd3
Luna Mendes <lkmnds@gmail.com>
2017-02-19 03:03:12 -0300

    locale: add json-merge.py

---
+++ locale/json-merge.py
@@ -0,0 +1,24 @@
+import sys
+import json
+
+def main(args):
+    basefile = args[1]
+    langfile = args[2]
+
+    basejson, langjson = None, None
+    with open(basefile, 'r') as f:
+        basejson = json.load(f)
+
+    with open(langfile, 'r') as f:
+        langjson = json.load(f)
+
+    # best merge ever!!!
+    for entry in basejson:
+        if entry not in langjson:
+            langjson[entry] = basejson[entry]
+
+    print(json.dump(langjson))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
