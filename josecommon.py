f959f232515bb2bbae53dc41c6d1a43522af15b7
Luna Mendes <lkmnds@gmail.com>
2017-02-24 03:22:46 -0300

    José 1.4.2

--- josecommon.py
+++ josecommon.py
@@ -32,7 +32,7 @@ logger.addHandler(handler)
 
 
 JOSE_PREFIX = "j!"
-JOSE_VERSION = '1.4.1'
+JOSE_VERSION = '1.4.2'
 
 MARKOV_LENGTH_PATH = 'db/wordlength.json'
 MARKOV_MESSAGES_PATH = 'db/messages.json'
