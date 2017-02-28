e05e1623b31acf0888a834b0b4eeb5426a67d2b9
Luna Mendes <lkmnds@gmail.com>
2017-02-28 16:03:57 -0300

    jose-bot.py: remove send_typing

--- jose-bot.py
+++ jose-bot.py
@@ -140,7 +140,6 @@ async def do_command(method, message, args, cxt, t_start, st):
         return
 
     try:
-        await cxt.send_typing()
         await jose_method(message, args, cxt)
 
     except je.PermissionError:
