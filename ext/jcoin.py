c2dab9242d67b3a0564b426d7c6b3d9c6a76e5b1
Luna Mendes <lkmnds@gmail.com>
2017-03-01 21:33:51 -0300

    jcoin: change cooldowns in steal command

--- ext/jcoin.py
+++ ext/jcoin.py
@@ -40,12 +40,19 @@ HELPTEXT_JC_STEAL = """
 use `j!stealstat` to see your status in the stealing business.
 
 The chance of getting caught increases more the more you steal.
+
+When using `j!steal`, `res` and `prob` show up, `res` is a random value and
+if it is greater than `prob`, you are arrested. `prob` is calculated using
+the target's current wallet and the amount you want to steal from them.
 """
 
 class JoseCoin(jaux.Auxiliar):
     def __init__(self, _client):
         jaux.Auxiliar.__init__(self, _client)
         self.counter = 0
+
+    def to_hours(self, seconds):
+        return seconds / 60 / 60
 
     async def josecoin_save(self, message, dbg_flag=True):
         res = self.jcoin.save('jcoin/josecoin.db')
@@ -367,15 +374,15 @@ class JoseCoin(jaux.Auxiliar):
             cooldown_sec, cooldown_type = cooldown
             cooldown_sec -= time.time()
             if cooldown_type == 0:
-                res.append(":cop: you're in prison, %d seconds remaining" % (cooldown_sec,))
+                res.append(":cop: you're in prison, %.2f hours remaining" % (self.to_hours(cooldown_sec),))
             elif cooldown_type == 1:
-                res.append(":alarm_clock: you're waiting for stealing points, %d seconds remaining" % (cooldown_sec,))
+                res.append(":alarm_clock: you're waiting for stealing points, %.2f hours remaining" % (self.to_hours(cooldown_sec),))
             else:
                 res.append(":warning: unknown cooldown type")
 
         if grace_period is not None:
             grace_period -= time.time()
-            res.append(":angel: you're in grace period, %d seconds remaining" % (grace_period,))
+            res.append(":angel: you're in grace period, %.2f hours remaining" % (self.to_hours(cooldown_sec),))
 
         await cxt.say('\n'.join(res))
 
@@ -415,10 +422,10 @@ class JoseCoin(jaux.Auxiliar):
             cooldown_end, cooldown_type = cdown
             remaining = cooldown_end - time.time()
             if cooldown_type == 0:
-                await cxt.say(":cop: You are still in prison, wait %d seconds", (remaining,))
+                await cxt.say(":cop: You are still in prison, wait %.2f hours", (self.to_hours(remaining),))
             elif cooldown_type == 1:
                 if remaining > 1:
-                    await cxt.say("Wait %d seconds to regenerate your stealing points", (remaining,))
+                    await cxt.say("Wait %.2f hours to regenerate your stealing points", (self.to_hours(remaining),))
                 else:
                     await cxt.say("Stealing points regenerated!")
                     del self.stealdb['points'][thief_id]
@@ -435,8 +442,8 @@ class JoseCoin(jaux.Auxiliar):
         grace_period = (self.stealdb['period'].get(target_id, 0) - time.time())
         if grace_period > 0:
             await cxt.say("Target is in :angel: grace period :angel:")
-            await cxt.say("%s tried to steal %.2fJC from you, but you have %d seconds of grace period", \
-                target_user, (str(thief_user), amount, grace_period))
+            await cxt.say("%s tried to steal %.2fJC from you, but you have %.2f hours of grace period", \
+                target_user, (str(thief_user), amount, self.to_hours(grace_period)))
             return
 
         if stealuses < 1:
@@ -445,8 +452,8 @@ class JoseCoin(jaux.Auxiliar):
             return
 
         if target_id == self.jcoin.jose_id:
-            await cxt.say(":cop: You can't steal from José. Arrested for 24h")
-            self.stealdb['cdown'][thief_id] = (time.time() + 86400, 0)
+            await cxt.say(":cop: You can't steal from José. Arrested for 8h")
+            self.stealdb['cdown'][thief_id] = (time.time() + 28800, 0)
             return
 
         target_account = self.jcoin.get(target_id)[1]
@@ -454,8 +461,8 @@ class JoseCoin(jaux.Auxiliar):
 
         if amount > target_amount:
             # automatically in prison
-            await cxt.say(":cop: Arrested because you tried to steal more than the target has, got 24h jailtime.")
-            self.stealdb['cdown'][thief_id] = (time.time() + 86400, 0)
+            await cxt.say(":cop: Arrested because you tried to steal more than the target has, 8h jail time.")
+            self.stealdb['cdown'][thief_id] = (time.time() + 28800, 0)
 
         D = decimal.Decimal
         chance = (BASE_CHANCE + (target_amount / amount)) * D(0.3)
@@ -472,10 +479,10 @@ class JoseCoin(jaux.Auxiliar):
             if not ok[0]:
                 await cxt.say("jc->err: %s", ok[1])
             else:
-                await cxt.say("`[res: %.2f < prob: %.2f]` Stealing went well, nobody noticed, you thief. Got %.2fJC from %s, \n`%s`", \
-                    (res, chance, amount, target_account['name'], ok[1]))
+                await cxt.say("`[res: %.2f < prob: %.2f]` Stealing went well, nobody noticed, you thief. \n`%s`", \
+                    (res, chance, ok[1]))
 
-                await cxt.say(":gun: You got robbed! The thief(%s) stole `%.2fJC` from you. 2 hour grace period", \
+                await cxt.say(":gun: You got robbed! The thief(%s) stole `%.2fJC` from you. 3 hour grace period", \
                     target_user, (str(thief_user), amount))
 
                 self.stealdb['period'][target_id] = time.time() + 10800
@@ -483,7 +490,7 @@ class JoseCoin(jaux.Auxiliar):
 
         else:
             # type 0 cooldown, you got arrested
-            await cxt.say("`[res: %.2f < prob: %.2f]` :cop: Arrested! got 24h cooldown on `j!steal`.", \
+            await cxt.say("`[res: %.2f > prob: %.2f]` :cop: Arrested! got 8h cooldown.", \
                 (res, chance))
             self.stealdb['cdown'][message.author.id] = (time.time() + 86400, 0)
 
