be7d2a3e7a28d7e91219b0e9f53cef92c1e5b316
Luna Mendes <lkmnds@gmail.com>
2017-03-11 00:01:09 -0300

    contrib: add jspeak-restore.py

---
+++ contrib/jspeak-restore.py
@@ -0,0 +1,30 @@
+import sqlite3
+import json
+import sys
+
+conn = sqlite3.connect('jose.db')
+
+def do_stmt(stmt, params=None):
+    global conn
+    cur = conn.cursor()
+    cur.execute(stmt, params)
+    conn.commit()
+    return cur
+
+def main(args):
+    sql_file = args[1]
+
+    print("Restoring jose.db markovdb table...")
+
+    stmts = 0
+    with open(sql_file, 'r') as sqlfile:
+        for line in sqlfile.readlines():
+            if stmts % 1000:
+                print("%d statements..." % stmts)
+            do_stmt(line)
+            stmts += 1
+
+    print("%d statements executed" % stmts)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
