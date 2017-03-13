67bcf19e7620c428afa97062d7e9785ac54b84ff
Luna Mendes <lkmnds@gmail.com>
2017-03-13 00:22:52 -0300

    José 1.5.1

--- josecommon.py
+++ josecommon.py
@@ -29,7 +29,7 @@ handler.setFormatter(formatter)
 # add the handlers to the logger
 logger.addHandler(handler)
 
-JOSE_VERSION = '1.5.0'
+JOSE_VERSION = '1.5.1'
 JOSE_PREFIX = "j!"
 LEN_PREFIX = len(JOSE_PREFIX)
 
