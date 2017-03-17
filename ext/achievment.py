9178e0baf77137dbe4b86110481a2c33551f2f74
Luna Mendes <lkmnds@gmail.com>
2017-03-17 01:17:32 -0300

    achievments: fix typos (2)

--- ext/achievment.py
+++ ext/achievment.py
@@ -84,11 +84,11 @@ class JoseAchievment(jaux.Auxiliar):
     async def c_achievments(self, message, args, cxt):
         achievments = self.achv_get(str(message.author.id))
         emoji = [ACHIEVMENT_EMOJI[a] for a in achievments]
-        await cxt.say("%s", (' '.join(emoji),))
+        await cxt.say(' '.join(emoji))
 
     async def c_listachv(self, message, args, cxt):
         await self.is_admin(message.author.id)
-        res = ['`{} - {}`'.format(k, ACHIEVMENT_NAMES[k]) for (k) in ACHIEVMENT_NAMES]
+        res = ['`{}` - {}\n'.format(k, ACHIEVMENT_NAMES[k]) for (k) in ACHIEVMENT_NAMES]
         await cxt.say("%s", ('\n'.join(res),))
 
     async def c_addachv(self, message, args, cxt):
