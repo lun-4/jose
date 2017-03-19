fd395f2bacc7b8375d22f9955e27df57fe8bdbfa
Luna Mendes <lkmnds@gmail.com>
2017-03-19 03:58:06 -0300

    jxtra: profile: make days and years calc independent

--- ext/josextra.py
+++ ext/josextra.py
@@ -386,7 +386,7 @@ Made with :heart: by Luna Mendes""" % (jcommon.JOSE_VERSION))
     def delta_str(self, delta):
         seconds = delta.total_seconds()
         years = seconds / 60 / 60 / 24 / 365.25
-        days = (seconds / 60 / 60 / 24) - (years * 365.25)
+        days = seconds / 60 / 60 / 24
         if years >= 1:
             return f'{years:.2f} years'
         else:
