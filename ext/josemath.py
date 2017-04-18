929f248329f7ca3d5b48230217b4ab0dcb160017
Luna Mendes <lkmnds@gmail.com>
2017-04-18 14:17:23 -0300

    jmath: pass None to _wg to get Kelvin temperature

--- ext/josemath.py
+++ ext/josemath.py
@@ -180,7 +180,7 @@ class JoseMath(jaux.Auxiliar):
 
         res = [f'{location}']
         res.append(f' - Status: `{status}` : {icon}')
-        res.append(f' - `{_wg("celsius")} °C, {_wg("fahrenheit")} °F, {_wg()} °K`')
+        res.append(f' - `{_wg("celsius")} °C, {_wg("fahrenheit")} °F, {_wg(None)} °K`')
 
         await cxt.say('\n'.join(res))
 
