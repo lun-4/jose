43eaa66909b711f947e4225528632e95b2f0176b
Luna Mendes <lkmnds@gmail.com>
2017-04-22 02:55:41 -0300

    stars: fix typo (2)

--- ext/stars.py
+++ ext/stars.py
@@ -587,8 +587,7 @@ class Stars(jaux.Auxiliar):
 
         _members = [
             ((discord.utils.get(self.client.get_all_members(), id=mid, server__id=server_id), n_stars) \
-                for (mid, n_stars) in top10_starrers
-        ]
+                for (mid, n_stars) in top10_starrers]
 
         stats.add_field(name='Most stars received',
                         value=f'{max_message[0]} Stars, ID {max_message[1]["message_id"]}')
