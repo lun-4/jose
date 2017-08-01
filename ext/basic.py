78b80efea33013216cecf0bd1d3c5786967a0973
Luna Mendes <lkmnds@gmail.com>
2017-07-31 22:59:24 -0300

    basic: add gay channel metric

--- ext/basic.py
+++ ext/basic.py
@@ -89,6 +89,10 @@ class Basic(Cog):
 
         em.add_field(name='Guilds', value=f'{len(self.bot.guilds)}')
         em.add_field(name='Channels', value=f'{channels}')
+
+        gay_chans = len(c for c in self.bot.get_all_channels() if 'gay' in c.name.lower())
+        em.add_field(name='Gaynnels', value=gay_chans)
+
         em.add_field(name='Texters', value=f'{len(self.bot.cogs["Speak"].text_generators)}/{len(self.bot.guilds)}')
         em.add_field(name='Members', value=len(list(self.bot.get_all_members())))
 
