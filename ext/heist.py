7e85df6ada27b4e39c4c01ae8495e96b9b8ed221
Luna Mendes <lkmnds@gmail.com>
2017-08-02 00:13:50 -0300

    heist: add GuildConverter

--- ext/heist.py
+++ ext/heist.py
@@ -5,6 +5,25 @@ from .common import Cog
 
 random = SystemRandom()
 
+class GuildConverter(commands.Converter):
+    async def guild_name_lookup(self, ctx, arg):
+        def f(guild):
+            return arg in guild.name.lower()
+        return discord.utils.get(f, ctx.bot.guilds)
+
+    async def convert(self, ctx, arg):
+        try:
+            guild_id = int(argument)
+        except (TypeError, ValueError):
+            guild = await self.guild_name_lookup(ctx, arg)
+            if guild is None:
+                raise commands.BadArgument('Guild not found')
+
+        guild = ctx.bot.get_guild(guild_id)
+        if guild is None:
+            raise commands.BadArgument('Guild not found')
+        return guild
+
 class Heist(Cog):
     """"""
 
