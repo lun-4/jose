107f91502133b995881d37b204c1d10c763b77bc
Luna Mendes <lkmnds@gmail.com>
2017-08-05 20:49:16 -0300

    heist: fix typo

--- ext/heist.py
+++ ext/heist.py
@@ -335,7 +335,7 @@ class Heist(Cog):
             if session.target.id == target.id:
                 raise self.SayException('An already existing session exists with the same target')
 
-        if target == guild:
+        if target == ctx.guild:
             return await ctx.send('lol')
 
         session = self.get_sess(ctx, target, True)
