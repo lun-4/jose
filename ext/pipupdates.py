908acd9af43e694b9638033757edb6b7e031b0b4
Luna Mendes <lkmnds@gmail.com>
2017-09-24 23:38:35 -0300

    pipupdates: use env

--- ext/pipupdates.py
+++ ext/pipupdates.py
@@ -10,7 +10,7 @@ from discord.ext import commands
 log = logging.getLogger(__name__)
 
 def pip_freeze():
-    out = subprocess.check_output('pip freeze', shell=True)
+    out = subprocess.check_output('env/bin/python -m pip freeze', shell=True)
     return out
 
 
@@ -54,30 +54,21 @@ class PipUpdates(Cog):
         res = []
 
         for pkgline in packages:
-            print('working on freeze pkgline', pkgline)
-
             r = pkgline.split('==')
             if len(r) != 2:
                 continue
             pkgname = r[0]
 
-            print('in requirements?', pkgname in self.requirements)
             if pkgname in self.requirements:
                 cur_version = self.requirements[pkgname]
 
-                print('requesting', f'http://pypi.python.org/pypi/{pkgname}/json')
                 pkgdata = await self.get_json(f'http://pypi.python.org/pypi/{pkgname}/json')
-
                 new_version = pkgdata['info']['version']
-                log.info('[pip] checking %s: current=%s against latest=%s',
-                         pkgname, cur_version, new_version)
 
                 if new_version != cur_version:
-                    log.info('[pip] New update for %s', pkgname)
                     res.append(" * `%r` needs update from %s to %s" % \
                         (pkgname, cur_version, new_version))
 
-        print('result is', res)
         await self.say_results(res)
         return res
 
