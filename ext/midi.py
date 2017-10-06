dff95aede9ab5065f920ca852106f6eea72851c0
FrostLuma <FrostLuma@gmail.com>
2017-10-07 00:30:10 +0200

    midi: fix midi extension, make midi command public

--- ext/midi.py
+++ ext/midi.py
@@ -1,11 +1,84 @@
-import time
+# -*- coding: utf-8 -*-
+import io
 import logging
+import time
 
+import discord
 from discord.ext import commands
+from midiutil import MIDIFile
 
 from .common import Cog
 
-log = logging.getLogger()
+
+LETTER_PITCH_MAP = {
+    "a": 34,
+    "A": 35,
+    "b": 36,
+    "B": 37,
+    "c": 38,
+    "C": 39,
+    "d": 40,
+    "D": 41,
+    "e": 42,
+    "E": 43,
+    "f": 44,
+    "F": 45,
+    "g": 46,
+    "G": 47,
+    "h": 48,
+    "H": 49,
+    "i": 50,
+    "I": 51,
+    "j": 52,
+    "J": 53,
+    "k": 54,
+    "K": 55,
+    "l": 56,
+    "L": 57,
+    "m": 58,
+    "M": 59,
+    "n": 60,
+    "N": 61,
+    "o": 62,
+    "O": 63,
+    "p": 64,
+    "P": 65,
+    "q": 66,
+    "Q": 67,
+    "r": 68,
+    "R": 69,
+    "s": 70,
+    "S": 71,
+    "t": 72,
+    "T": 73,
+    "u": 74,
+    "U": 75,
+    "v": 76,
+    "V": 77,
+    "w": 78,
+    "W": 79,
+    "x": 80,
+    "X": 81,
+    "y": 82,
+    "Y": 83,
+    "z": 84,
+    "Z": 85,
+
+    "0": 86,
+    "1": 87,
+    "2": 88,
+    "3": 89,
+    "4": 90,
+    "5": 91,
+    "6": 92,
+    "7": 93,
+    "8": 94,
+    "9": 95,
+}
+
+
+log = logging.getLogger(__name__)
+
 
 async def get_duration(letter):
     return {
@@ -14,59 +87,56 @@ async def get_duration(letter):
         '.': 4,
     }.get(letter, 1)
 
-async def get_note(letter):
-    # TODO: letter_pitch_mapping
-    return letter_pitch_mapping.get(letter)
 
 class MIDI(Cog):
-    async def make_midi(self, tempo, data):
-        midifile = MIDIFile(1)
+    async def make_midi(self, tempo: int, data: str) -> MIDIFile:
+        midi_file = MIDIFile(1)
 
-        midifile.addTrackName(0, 0, 'ur gay')
-        midifile.addTempo(0, 0, tempo)
+        midi_file.addTrackName(0, 0, 'beep boop')
+        midi_file.addTempo(0, 0, tempo)
 
-        log.info('Making MIDI out of %r', data)
+        log.info(f'creating MIDI out of "{data}"')
 
         for index, letter in enumerate(data):
-            note = await get_note(letter)
+            note = LETTER_PITCH_MAP.get(letter)
             if note is None:
                 continue
 
             # modifiers are characters before the actual
             # letter note that modify the note's duration
-
             try:
                 duration = await get_duration(data[index - 1])
-            except IndexError: duration = 1
+            except IndexError:
+                duration = 1
 
-            await self.loop.run_in_executor(None, midifile.add_one,\
-                0, 0, note, index, duration, 100)
+            await self.loop.run_in_executor(
+                None, midi_file.addNote, 0, 0, note, index, duration, 100
+            )
 
-
-        return midifile
+        log.info('successfully created MIDI')
+        return midi_file
 
     @commands.command()
-    @commands.is_owner()
-    async def midi(self, ctx, tempo: int=120, *, data: str):
-        """Convert text to MIDI.
-        
+    async def midi(self, ctx: commands.Context, tempo: int=120, *, data: str):
+        """
+        Convert text to MIDI.
+
         The actual letter-to-note map? idk :shrug:
         """
-        t1 = time.monotonic()
-        midifile = await self.make_midi(tempo, data)
-        t2 = time.monotonic()
+        before = time.monotonic()
+        midi_file = await self.make_midi(tempo, data)
+        duration = (time.monotonic() - before) * 1000
 
-        if midifile is None:
-            return await ctx.send('Failed to generate MIDI file')
+        if midi_file is None:
+            return await ctx.send('Failed to generate a MIDI file!')
 
-        delta = round((t2 - t1) * 1000, 2)
+        file = io.BytesIO()
+        await self.loop.run_in_executor(None, midi_file.writeFile, file)
+        file.seek(0)
 
-        fp = io.BytesIO()
-        await self.loop.run_in_executor(None, midifile.writeFile, fp)
+        wrapped = discord.File(file, filename='boop.midi')
+        await ctx.send(f'Took {duration:.3f}ms!', file=wrapped)
 
-        wrapped = discord.File(fp, filename='gay.midi')
-        await ctx.send(f'took {delta}ms', file=wrapped)
 
-def setup(bot):
+def setup(bot: commands.Bot):
     bot.add_cog(MIDI(bot))
-
