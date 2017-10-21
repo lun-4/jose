ec9c488cf84238d24b23e5999f3421632c60d9fd
Luna Mendes <lkmnds@gmail.com>
2017-10-21 02:38:43 -0300

    coins+: lol

--- ext/coins+.py
+++ ext/coins+.py
@@ -28,7 +28,8 @@ ARREST_TIME = 6
 # cooldown when you need to regen stealing points
 STEAL_REGEN = 9
 
-WHITELISTED_ACCOUNTS = (319522792854913025,)
+WHITELISTED_ACCOUNTS = (319522792854913025,
+                        202587271679967232)
 
 
 def make_default_points(ctx):
