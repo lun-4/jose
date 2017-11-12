54fe1a66f3235e89f4b4ad5eae6a64a0c538a895
Luna Mendes <lkmnds@gmail.com>
2017-11-12 00:09:44 -0300

    rpg: force str

--- ext/rpg.py
+++ ext/rpg.py
@@ -88,7 +88,7 @@ class RPG(Cog):
             e.set_thumbnail(url=person.avatar_url)
 
         # calculate XP and levels
-        e.add_field(name='Level', value=self.get_level(inv))
+        e.add_field(name='Level', value=str(self.get_level(inv)))
 
         xp_next_level = self.get_next_level_xp(inv)
         e.add_field(name='XP', value=f'{inv["xp"]} / {xp_next_level} XP')
