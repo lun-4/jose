9f4deba7f60bd1b9ccaff371aaea3610e2d45dd0
Luna Mendes <lkmnds@gmail.com>
2017-11-13 01:25:47 -0300

    admin: accept lists of exts

--- ext/admin.py
+++ ext/admin.py
@@ -20,36 +20,39 @@ class Admin(Cog):
 
     @commands.command(hidden=True)
     @commands.is_owner()
-    async def load(self, ctx, extension_name: str):
+    async def load(self, ctx, *exts: str):
         """Loads an extension."""
-        try:
-            self.bot.load_extension("ext." + extension_name)
-        except Exception as e:
-            await ctx.send(f'```py\n{traceback.format_exc()}\n```')
-            return
-        log.info(f'Loaded {extension_name}')
-        await ctx.send(f':ok_hand: `{extension_name}` loaded.')
+        for ext in exts:
+            try:
+                self.bot.load_extension("ext." + ext)
+            except Exception as e:
+                await ctx.send(f'Oops. ```py\n{traceback.format_exc()}\n```')
+                return
+            log.info(f'Loaded {ext}')
+            await ctx.send(f':ok_hand: `{ext}` loaded.')
 
     @commands.command(hidden=True)
     @commands.is_owner()
-    async def unload(self, ctx, extension_name: str):
+    async def unload(self, ctx, *exts: str):
         """Unloads an extension."""
-        self.bot.unload_extension('ext.' + extension_name)
-        log.info(f'Unloaded {extension_name}')
-        await ctx.send(f':ok_hand: `{extension_name}` unloaded.')
+        for ext in exts:
+            self.bot.unload_extension('ext.' + ext)
+            log.info(f'Unloaded {ext}')
+            await ctx.send(f':ok_hand: `{ext}` unloaded.')
 
     @commands.command(hidden=True)
     @commands.is_owner()
-    async def reload(self, ctx, extension_name: str):
+    async def reload(self, ctx, *extensions: str):
         """Reloads an extension"""
-        try:
-            self.bot.unload_extension('ext.' + extension_name)
-            self.bot.load_extension('ext.' + extension_name)
-        except Exception as err:
-            await ctx.send(f'```{traceback.format_exc()}```')
-            return
-        log.info(f'Reloaded {extension_name}')
-        await ctx.send(f':ok_hand: Reloaded `{extension_name}`')
+        for ext in extensions:
+            try:
+                self.bot.unload_extension('ext.' + ext)
+                self.bot.load_extension('ext.' + ext)
+            except Exception as err:
+                await ctx.send(f'```{traceback.format_exc()}```')
+                return
+            log.info(f'Reloaded {ext}')
+            await ctx.send(f':ok_hand: Reloaded `{ext}`')
 
     @commands.command()
     @commands.is_owner()
