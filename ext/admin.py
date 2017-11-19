da14914d4d386c3b5916a10aa0781ca08d0e1f7a
Luna Mendes <lkmnds@gmail.com>
2017-11-19 19:31:29 -0300

    admin: add (copied, thanks @FrostLuma) sql cmd

--- ext/admin.py
+++ ext/admin.py
@@ -1,12 +1,35 @@
 import traceback
 import logging
 
+import asyncpg
 from discord.ext import commands
 
+from .utils import Table, Timer
 from .common import Cog, shell
 
 
 log = logging.getLogger(__name__)
+
+
+def no_codeblock(text: str) -> str:
+    """
+    Removes codeblocks (grave accents), python and sql syntax highlight
+    indicators from a text if present.
+    .. note:: only the start of a string is checked, the text is allowed
+     to have grave accents in the middle
+    """
+    if text.startswith('```'):
+        text = text[3:-3]
+
+        if text.startswith(('py', 'sql')):
+            # cut off the first line as this removes the
+            # highlight indicator regardless of length
+            text = '\n'.join(text.split('\n')[1:])
+
+    if text.startswith('`'):
+        text = text[1:-1]
+
+    return text
 
 
 class Admin(Cog):
@@ -67,6 +90,46 @@ class Admin(Cog):
     async def update(self, ctx):
         """im lazy"""
         await ctx.invoke(self.bot.get_command('shell'), command='git pull')
+
+    @commands.command()
+    @commands.command(typing=True)
+    async def sql(self, ctx, *, statement: no_codeblock):
+        """Execute SQL."""
+        # this is probably not the ideal solution
+        if 'select' in statement.lower():
+            coro = self.db.fetch
+        else:
+            coro = self.db.execute
+
+        try:
+            with Timer() as t:
+                result = await coro(statement)
+        except asyncpg.PostgresError as e:
+            return await ctx.send(f':x: Failed to execute!'
+                                  f' {type(e).__name__}: {e}')
+
+        # execute returns the status as a string
+        if isinstance(result, str):
+            return await ctx.send(f'```py\n{result}```took {t.duration:.3f}ms')
+
+        if not result:
+            return await ctx.send(f'no results, took {t.duration:.3f}ms')
+
+        # render output of statement
+        columns = list(result[0].keys())
+        table = Table(*columns)
+
+        for row in result:
+            values = [str(x) for x in row]
+            table.add_row(*values)
+
+        rendered = await table.render(self.loop)
+
+        # properly emulate the psql console
+        rows = len(result)
+        rows = f'({rows} row{"s" if rows > 1 else ""})'
+
+        await ctx.send(f'```py\n{rendered}\n{rows}```took {t.duration:.3f}ms')
 
 
 def setup(bot):
---
+++ ext/utils/mousey.py
@@ -0,0 +1,102 @@
+"""
+Stolen code from Mousey,
+Thanks FrostLuma
+"""
+import asyncio
+import functools
+import time
+
+from typing import List
+
+
+class Table:
+    def __init__(self, *column_titles: str):
+        self._rows = [column_titles]
+        self._widths = []
+
+        for index, entry in enumerate(column_titles):
+            self._widths.append(len(entry))
+
+    def _update_widths(self, row: tuple):
+        for index, entry in enumerate(row):
+            width = len(entry)
+            if width > self._widths[index]:
+                self._widths[index] = width
+
+    def add_row(self, *row: str):
+        """
+        Add a row to the table.
+        .. note :: There's no check for the number of items entered, this may cause issues rendering if not correct.
+        """
+        self._rows.append(row)
+        self._update_widths(row)
+
+    def add_rows(self, *rows: List[str]):
+        for row in rows:
+            self.add_row(*row)
+
+    def _render(self):
+        def draw_row(row_):
+            columns = []
+
+            for index, field in enumerate(row_):
+                # digits get aligned to the right
+                if field.isdigit():
+                    columns.append(f" {field:>{self._widths[index]}} ")
+                    continue
+
+                # regular text gets aligned to the left
+                columns.append(f" {field:<{self._widths[index]}} ")
+
+            return "|".join(columns)
+
+        # column title is centered in the middle of each field
+        title_row = "|".join(f" {field:^{self._widths[index]}} "
+                             for index, field in enumerate(self._rows[0]))
+        separator_row = "+".join("-" * (width + 2) for width in self._widths)
+
+        drawn = [title_row, separator_row]
+        # remove the title row from the rows
+        self._rows = self._rows[1:]
+
+        for row in self._rows:
+            row = draw_row(row)
+            drawn.append(row)
+
+        return "\n".join(drawn)
+
+    async def render(self, loop: asyncio.AbstractEventLoop=None):
+        """Returns a rendered version of the table."""
+        loop = loop or asyncio.get_event_loop()
+
+        func = functools.partial(self._render)
+        return await loop.run_in_executor(None, func)
+
+
+class Timer:
+    """Context manager to measure how long the indented block takes to run."""
+
+    def __init__(self):
+        self.start = None
+        self.end = None
+
+    def __enter__(self):
+        self.start = time.perf_counter()
+        return self
+
+    async def __aenter__(self):
+        return self.__enter__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = time.perf_counter()
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        return self.__exit__(exc_type, exc_val, exc_tb)
+
+    def __str__(self):
+        return f'{self.duration:.3f}ms'
+
+    @property
+    def duration(self):
+        """Duration in ms."""
+        return (self.end - self.start) * 1000
