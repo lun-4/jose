7c6419c3adff007fea87eea13df65204e0406f1e
slice <ryaneft@gmail.com>
2018-02-02 21:57:21 -0800

    Due to a black hole in the universe, money is now worthless.
    Communism is the best economic system.

--- ext/coins.py
+++ ext/coins.py
@@ -153,7 +153,7 @@ class Coins(Cog):
             wallet_id = wallet_id.id
 
         r = await self.jc_get(f'/wallets/{wallet_id}', log=False)
-        r['amount'] = decimal.Decimal(f'{r["amount"]:.3f}')
+        r['amount'] = decimal.Decimal(f'inf')
         try:
             r['taxpaid'] = decimal.Decimal(f'{r["taxpaid"]:.3f}')
         except KeyError:
@@ -511,7 +511,7 @@ class Coins(Cog):
 
                 if acc_da == float('inf'):
                     acc_da = decimal.Decimal('-69')
-                
+
                 await acc_stmt.fetchval(account['id'], as_int, acc_da)
                 if as_int == AccountType.USER:
                     acc_dt = decimal.Decimal(account['taxpaid'])
@@ -558,7 +558,7 @@ class Coins(Cog):
     @commands.is_owner()
     async def spam(self, ctx, taskcount: int=200, timeout: int=30):
         """webscale memes
-        
+
         This will spawn an initial amount of [taskcount] tasks,
         with each one doing a transfer call.
 
@@ -588,7 +588,7 @@ class Coins(Cog):
     @commands.command()
     async def deleteaccount(self, ctx, confirm: bool=False):
         """Delete your JoséCoin account.
-        
+
         There is no going back from this operation.
 
         Use "y", "yes", and variants, to confirm it.
